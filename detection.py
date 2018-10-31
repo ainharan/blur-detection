924539ab205a1bdf1e77d9065d045bf3f59f71c7
ainharan <ainharan.subramaniam@gmail.com>
2018-10-31 20:37:25 +1100

    some clean up and prints cross-validation results

--- detection.py
+++ detection.py
@@ -22,26 +22,10 @@ def get_data():
     blurry_list = pp.get_training_data(pp.blurry_path)
     good_list = pp.get_training_data(pp.good_path)
 
-#    #seperate into training and test sets 80:20
-#    # different ratios in the case we are using different data sets
-#    split_f = int(len(flare_list) * 0.8)    #split ratio for flares
-#    split_b = int(len(blurry_list) * 0.8)    #split ratio for blurry
-#    split_g = int(len(good_list) * 0.8)    #split ratio for good
-#
-#    flist_train, flist_test = flare_list[:split_f], flare_list[split_f:]
-#    blist_train, blist_test = blurry_list[:split_b], blurry_list[split_b:]
-#    glist_train, glist_test = good_list[:split_g], good_list[split_g:]
-#
-#    paths = [pp.flare_path]*len(flist_test)
-#    paths = paths + [pp.blurry_path]*len(blist_test)
-#    paths = paths + [pp.good_path]*len(glist_test)
-
     paths = [pp.flare_path]*len(flare_list)
     paths = paths + [pp.blurry_path]*len(blurry_list)
     paths = paths + [pp.good_path]*len(good_list)
 
-
-#    test = flist_test + blist_test + glist_test
     test = flare_list + blurry_list + good_list
 
     # label flare files as 1 and add to training set
@@ -88,20 +72,33 @@ def read_img(path, file):
 
     return imrgb
 
+def calculate_accuracy(prediction, correct, accuracy):
+    if prediction == 1 and correct == "flare":
+        accuracy = accuracy +1
+    if prediction == 2 and correct == "blurry":
+        accuracy = accuracy +1
+    if prediction == 3 and correct == "good":
+        accuracy = accuracy +1
+
+    return accuracy
+
+
 
 def main():
     training, labels, test, paths = get_data()
     test_arg = []
     h = Hog()
 
+    kfold = 1
+    svm_overall = 0
+    knn_overall = 0
     skf = StratifiedKFold(n_splits=5)
     for train_index, test_index in skf.split(training, labels):
-#        print "TRAIN:", train_index, "TEST:", test_index
         X_train, X_test = training[train_index], training[test_index]
         y_train, y_test = labels[train_index], labels[test_index]
- #       print X_train, X_test
         test_set = []
-        accuracy = 0
+        svm_sum = 0
+        knn_sum = 0
 
         # SVM model
         svm = cv2.ml.SVM_create()
@@ -111,8 +108,8 @@ def main():
         svm.setKernel(cv2.ml.SVM_LINEAR)
         svm.train(X_train, cv2.ml.ROW_SAMPLE, y_train) 
 
-        model = cv2.ml.KNearest_create()
-        model.train(X_train, cv2.ml.ROW_SAMPLE, y_train)
+        knn = cv2.ml.KNearest_create()
+        knn.train(X_train, cv2.ml.ROW_SAMPLE, y_train)
 
         path_files = [paths[i] for i in test_index]
         test_files = [test[i] for i in test_index]
@@ -124,45 +121,61 @@ def main():
                 continue
 
             # gets the label of current image for accuracy calculation
-            correct = path.split('/')[2].split('-')[0]
+            l = path.split('/')[2].split('-')[0]
             r_img=cv2.resize(img,(400,300))
             hist=h.hog(r_img)
             test_set.append(hist)
             test_data = np.float32(test_set)
             result = svm.predict(test_data)
-            retval, knnresults, neigh_resp, dists = model.findNearest(test_data, 3)
-            kek = len(result)
-#            print knnresults.ravel()
-            prediction =  int(result[1][-1][0])
-            if prediction == 1 and correct == "flare":
-                accuracy = accuracy +1
-            if prediction == 2 and correct == "blurry":
-                accuracy = accuracy +1
-            if prediction == 3 and correct == "good":
-                accuracy = accuracy +1
+            retval, knnresults, neigh_resp, dists = knn.findNearest(test_data, 3)
+            prediction_svm =  int(result[1][-1][0])
+            prediction_knn = int(knnresults[-1][0])
+            svm_sum = calculate_accuracy(prediction_svm, l, svm_sum)
+            knn_sum = calculate_accuracy(prediction_knn, l, knn_sum)
 
-        print result[1].ravel()
-        accuracy = float(accuracy) / float(len(result[1]))
-        print accuracy
+        print "---------------- k=", kfold, " ----------------"
+        print "SVM predictions: " , result[1].ravel()
+        print "KNN predictions: " , knnresults.ravel()
+        print "Actual labels for test set:" , labels[test_index]
+        svm_acc = float(svm_sum) / float(len(result[1]))
+        knn_acc = float(knn_sum) / float(len(knnresults.ravel()))
+        print "Accuracy of svm: ", svm_acc
+        print "Accuracy of knn: ", knn_acc
 
-#    if len(sys.argv) > 1:
-#        file = sys.argv[1].split('/')[-1]
-#        path = os.path.dirname(os.path.abspath(sys.argv[1]))
-#        arg_img = read_img(path, file)
-#        if arg_img is None:
-#            print "The image passed in invalid, bad path or corrupt file"
-#
-#        # gets the label of current image for accuracy calculation
-#        r_img=cv2.resize(arg_img,(400,300))
-#        hist=h.hog(r_img)
-#        test_arg.append(hist)
-#        test_data = np.float32(test_arg)
-#        result = svm.predict(test_data)
-#        prediction =  int(result[1][-1][0])
-#        print prediction
-#    else:
-#        print "Usage: ./detection.py ./path/to/image.jpg"
-#
+        svm_overall = svm_acc + svm_overall
+        knn_overall = knn_acc + knn_overall
+        kfold = kfold + 1
+
+    svm_avg = float(svm_overall)/float(kfold-1)
+    knn_avg = float(knn_overall)/float(kfold-1)
+
+    print "Avg accuracy of svm after k=",kfold-1, ": ", svm_avg
+    print "Avg accuracy of knn after k=",kfold-1, ": ", knn_avg
+
+
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 3 and sys.argv[2] == "--show-results":
+            print "TO DO"
+        file = sys.argv[1].split('/')[-1]
+        path = os.path.dirname(os.path.abspath(sys.argv[1]))
+        arg_img = read_img(path, file)
+        if arg_img is None:
+            print "The image passed in invalid, bad path or corrupt file"
+
+        # gets the label of current image for accuracy calculation
+        r_img=cv2.resize(arg_img,(400,300))
+        hist=h.hog(r_img)
+        test_arg.append(hist)
+        test_data = np.float32(test_arg)
+        result = svm.predict(test_data)
+        retval, knnresults, neigh_resp, dists = knn.findNearest(test_data, 3)
+        prediction =  int(result[1][-1][0])
+#       prediction_knn = int(knnresults[-1][0])
+        print prediction
+#        print prediction_knn
+    else:
+        print "Usage: ./detection.py ./path/to/image.jpg"
+
 
 if __name__ == "__main__":
     main()
