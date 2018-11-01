3c6468ca7b0644fb61185b2b5d7e9d02e4b7321d
ainharan <ainharan.subramaniam@gmail.com>
2018-11-01 16:55:13 +1100

    renamed data folder as training data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,9 +3,9 @@ import os
 class Preprocessing(object):
 
     def __init__(self):
-        self.flare_path = '../data/flare-data/'
-        self.blurry_path = '../data/blurry-data/'
-        self.good_path = '../data/good-data/'
+        self.flare_path = '../training-data/flare-data/'
+        self.blurry_path = '../training-data/blurry-data/'
+        self.good_path = '../training-data/good-data/'
 
     def get_training_data(self, path):
         '''
